68e026d0eb886a2364383bd1f8a823d470d84a88
Alexandre Annic <alexandre.annic1@gmail.com>
2023-09-18 09:29:24 +0300

    continue

--- src/feature/kobo/KoboApiService.ts
+++ src/feature/kobo/KoboApiService.ts
@@ -7,6 +7,7 @@ import {UUID} from '../../core/Type'
 import {Arr, Enum} from '@alexandreannic/ts-utils'
 
 export class KoboApiService {
+
   constructor(
     private prisma: PrismaClient,
     private koboSdkGenerator: KoboSdkGenerator = new KoboSdkGenerator(prisma),
@@ -14,136 +15,10 @@ export class KoboApiService {
   ) {
   }
 
-  readonly constructSdk = (serverId: string): Promise<KoboSdk> => this.koboSdkGenerator.construct(serverId)
-
-  /** @deprecated?*/
-  readonly saveApiFormToDb = async (serverId: string, formId: string): Promise<KoboForm> => {
-    const maybeSavedForm = await this.prisma.koboForm.findFirst({where: {id: formId}})
-    if (maybeSavedForm) return maybeSavedForm
-    const sdk = await this.koboSdkGenerator.construct(serverId)
-    const form = await sdk.getForm(formId)
-    return this.prisma.koboForm.create({
-      data: {
-        id: formId,
-        name: form.name,
-        serverId,
-      }
-    })
-  }
+  readonly constructSdk = (serverId: string): Promise<KoboSdk> => this.koboSdkGenerator.get(serverId)
 
   readonly fetchAnswers = async (serverId: string, formId: string, params: KoboAnswerParams = {}) => {
-    const sdk = await this.koboSdkGenerator.construct(serverId)
+    const sdk = await this.koboSdkGenerator.get(serverId)
     return sdk.getAnswers(formId, params)
-  }
-
-  /** @deprecated since it duplicates uuid */
-  private readonly softDelete = (serverId: UUID, formId: KoboId, uuids: UUID[]) => {
-    return this.prisma.koboAnswers.findMany({where: {id: {notIn: uuids}}}).then(res => {
-      return Promise.all(res.map(_ => {
-        return this.prisma.koboAnswers.updateMany({
-          where: {
-            id: _.id,
-            source: serverId
-          },
-          data: {
-            deletedAt: new Date(),
-            deletedBy: 'Server sync',
-          }
-        })
-      }))
-    })
-  }
-
-  private readonly hardDelete = (uuids: UUID[]) => {
-    return this.prisma.koboAnswers.findMany({where: {id: {notIn: uuids}}}).then(res => {
-      return Promise.all(res.map(_ => {
-        return this.prisma.koboAnswers.deleteMany({
-          where: {
-            id: _.id,
-          },
-        })
-      }))
-    })
-  }
-
-  readonly syncAllApiAnswersToDb = async () => {
-    const allForms = await this.prisma.koboForm.findMany()
-    this.log.info(`Synchronize kobo forms:`)
-    for (const form of allForms) {
-      this.log.info(`Synchronizing ${form.name} (${form.id}...`)
-      await this.syncApiAnswersToDb(form.serverId, form.id)
-      this.log.info(`Synchronizing ${form.name} (${form.id} completed.`)
-    }
-    this.log.info(`Synchronize kobo forms completed!`)
-  }
-
-  readonly syncApiAnswersToDb = async (serverId: string, formId: string) => {
-    const sdk = await this.koboSdkGenerator.construct(serverId)
-    const remoteAnswers = await sdk.getAnswers(formId).then(_ => _.data)
-
-    const localAnswersIndex = await this.prisma.koboAnswers.findMany({where: {formId}, select: {id: true}}).then(_ => {
-      return new Set(_.map(_ => _.id))
-    })
-
-    const handleDelete = async () => {
-      const remoteIdsIndex = new Set(remoteAnswers.map(_ => _.id))
-      const idsToDelete = [...localAnswersIndex].filter(_ => !remoteIdsIndex.has(_))
-      await this.prisma.koboAnswers.deleteMany({where: {formId, id: {in: idsToDelete}}})
-    }
-
-    const handleCreate = async () => {
-      const notInsertedAnswers = remoteAnswers.filter(_ => !localAnswersIndex.has(_.id))
-      const inserts = notInsertedAnswers.map(_ => {
-        const res: Prisma.KoboAnswersUncheckedCreateInput = {
-          formId,
-          answers: _.answers,
-          id: _.id,
-          uuid: _.uuid,
-          start: _.start,
-          end: _.end,
-          submissionTime: _.submissionTime,
-          validationStatus: _.validationStatus,
-          lastValidatedTimestamp: _.lastValidatedTimestamp,
-          validatedBy: _.validatedBy,
-          version: _.version,
-          source: serverId,
-          attachments: _.attachments,
-        }
-        return res
-      })
-      return this.prisma.koboAnswers.createMany({
-        data: inserts,
-        skipDuplicates: true,
-      })
-    }
-
-    const handleUpdate = async () => {
-      const answersToUpdate = remoteAnswers.filter(_ => localAnswersIndex.has(_.id))
-      await Promise.all(answersToUpdate.map(a => {
-        return this.prisma.koboAnswers.update({
-          where: {
-            id: a.id,
-          },
-          data: {
-            uuid: a.uuid,
-            validationStatus: a.validationStatus,
-            submissionTime: a.submissionTime,
-            answers: a.answers,
-          }
-        })
-      }))
-    }
-
-    await handleDelete()
-    await handleCreate()
-    await handleUpdate()
-    await this.prisma.koboForm.update({
-      where: {
-        id: formId,
-      },
-      data: {
-        updatedAt: new Date(),
-      }
-    })
   }
 }
---
+++ src/feature/kobo/KoboSyncServer.ts
@@ -0,0 +1,116 @@
+import {UUID} from '../../core/Type'
+import {KoboId} from '../connector/kobo/KoboClient/type/KoboAnswer'
+import {Prisma, PrismaClient} from '@prisma/client'
+import {KoboSdkGenerator} from './KoboSdkGenerator'
+import {logger, Logger} from '../../helper/Logger'
+
+export class KoboSyncServer {
+
+  constructor(
+    private prisma: PrismaClient,
+    private koboSdkGenerator: KoboSdkGenerator = new KoboSdkGenerator(prisma),
+    private log: Logger = logger('KoboApiService')
+  ) {
+  }
+
+  readonly syncAllApiAnswersToDb = async () => {
+    const allForms = await this.prisma.koboForm.findMany()
+    this.log.info(`Synchronize kobo forms:`)
+    for (const form of allForms) {
+      this.log.info(`Synchronizing ${form.name} (${form.id}...`)
+      await this.syncApiForm(form.serverId, form.id)
+      this.log.info(`Synchronizing ${form.name} (${form.id} completed.`)
+    }
+    this.log.info(`Synchronize kobo forms completed!`)
+  }
+
+  readonly syncApiForm = async (serverId: UUID, formId: KoboId) => {
+    await this.syncApiFormInfo(serverId, formId)
+    await this.syncApiFormAnswers(serverId, formId)
+    await this.prisma.koboForm.update({
+      where: {id: formId},
+      data: {updatedAt: new Date()}
+    })
+  }
+
+  private readonly syncApiFormInfo = async (serverId: UUID, formId: KoboId) => {
+    const sdk = await this.koboSdkGenerator.get(serverId)
+    const form = await sdk.getForm(formId)
+    return this.prisma.koboForm.update({
+      where: {id: formId},
+      data: {name: form.name}
+    })
+  }
+
+  private readonly syncApiFormAnswers = async (serverId: UUID, formId: KoboId) => {
+    const sdk = await this.koboSdkGenerator.get(serverId)
+    const remoteAnswers = await sdk.getAnswers(formId).then(_ => _.data)
+
+    const localAnswersIndex = await this.prisma.koboAnswers.findMany({where: {formId}, select: {id: true}}).then(_ => {
+      return new Set(_.map(_ => _.id))
+    })
+
+    const handleDelete = async () => {
+      const remoteIdsIndex = new Set(remoteAnswers.map(_ => _.id))
+      const idsToDelete = [...localAnswersIndex].filter(_ => !remoteIdsIndex.has(_))
+      await this.prisma.koboAnswers.deleteMany({where: {formId, id: {in: idsToDelete}}})
+    }
+
+    const handleCreate = async () => {
+      const notInsertedAnswers = remoteAnswers.filter(_ => !localAnswersIndex.has(_.id))
+      const inserts = notInsertedAnswers.map(_ => {
+        const res: Prisma.KoboAnswersUncheckedCreateInput = {
+          formId,
+          answers: _.answers,
+          id: _.id,
+          uuid: _.uuid,
+          start: _.start,
+          end: _.end,
+          submissionTime: _.submissionTime,
+          validationStatus: _.validationStatus,
+          lastValidatedTimestamp: _.lastValidatedTimestamp,
+          validatedBy: _.validatedBy,
+          version: _.version,
+          source: serverId,
+          attachments: _.attachments,
+        }
+        return res
+      })
+      return this.prisma.koboAnswers.createMany({
+        data: inserts,
+        skipDuplicates: true,
+      })
+    }
+  }
+
+  /** @deprecated because it duplicates uuid */
+  private readonly softDelete = (serverId: UUID, formId: KoboId, uuids: UUID[]) => {
+    return this.prisma.koboAnswers.findMany({where: {id: {notIn: uuids}}}).then(res => {
+      return Promise.all(res.map(_ => {
+        return this.prisma.koboAnswers.updateMany({
+          where: {
+            id: _.id,
+            source: serverId
+          },
+          data: {
+            deletedAt: new Date(),
+            deletedBy: 'Server sync',
+          }
+        })
+      }))
+    })
+  }
+
+  /** @deprecated because it duplicates uuid */
+  private readonly hardDelete = (uuids: UUID[]) => {
+    return this.prisma.koboAnswers.findMany({where: {id: {notIn: uuids}}}).then(res => {
+      return Promise.all(res.map(_ => {
+        return this.prisma.koboAnswers.deleteMany({
+          where: {
+            id: _.id,
+          },
+        })
+      }))
+    })
+  }
+}
